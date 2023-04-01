6a2d3c90ac764c20dc61329614245f4938a76e50
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-04-01 05:50:14 +0200

    add new projects to portfolio pages, delete sweet alert code from javascript code

--- js/contact.js
+++ js/contact.js
@@ -4,42 +4,8 @@ let inputs = [
   document.getElementById("name"),
   document.getElementById("email"),
   document.getElementById("subject"),
-  document.getElementById("msg"),
+  document.getElementById("msg")
 ];
-
-// Sweet alert
-
-// class Sweet {
-//   constructor(alert, inputs) {
-//     this.alert = alert;
-//     this.inputs = inputs;
-//   }
-
-//   alertFunction() {
-//     let tarInput;
-//     this.inputs.forEach(el => {
-//       el.addEventListener("blur", function() {
-//         tarInput = this.getAttribute('id');
-//         console.log(this.alert.name);
-//         let targetAlert = this.alert[`${tarInput}`];
-//         targetAlert.style.display = "block";
-//       });
-//     });
-//   }
-// }
-
-// let sweetAlert = new Sweet({
-//   name: document.querySelectorAll(".sweet-alert")[0],
-//   email: document.querySelectorAll(".sweet-alert")[1],
-//   msg: document.querySelectorAll(".sweet-alert")[2],
-// },inputs);
-
-// inputs.forEach((el) => {
-//   el.addEventListener("blur", function () {
-//     if (this.value == "") sweetAlert.alertFunction();
-//     else console.log(true);
-//   });
-// });
 
 let validRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
