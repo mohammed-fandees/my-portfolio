06409b6d26f914ba86922ce137e9b7409627b1e5
Mohammed Fandees <mohammed.fandees@gmail.com>
2023-08-03 15:37:45 +0300

    remove some unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, About, Portfolio, Contact } from "./pages";
 import { Footer, Header } from "./components";
-import { useEffect, useState } from "react";
-import { getPath } from "./components/Header/Header";
+
 
 export default function App() {
-  const currentPath = window.location.pathname;
-  const [path, setPath] = useState(currentPath);
-
-  useEffect(() => {
-    setPath(getPath());
-    console.log("Changed")
-  }, [currentPath]);
-
   return (
     <Router>
       <Header />
@@ -22,7 +13,7 @@ export default function App() {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      {!path === "/" ? <Footer /> : null}
+      <Footer />
     </Router>
   );
 }
